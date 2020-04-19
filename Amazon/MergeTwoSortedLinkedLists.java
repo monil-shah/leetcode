7376210287a9581a6f9bbf4482d985756f6631ef
msmonilshah007 <mds747@nyu.edu>
2020-04-19 17:31:25 -0400

    Added code

---
+++ Amazon/MergeTwoSortedLinkedLists.java
@@ -0,0 +1,37 @@
+/*Merge Two Sorted Lists
+Solution
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(-1);
+        ListNode curr = res;
+        while(l1 != null && l2 != null){
+            if(l1.val<l2.val){
+                curr.next = l1;
+                l1 = l1.next;
+            }
+            else{
+                curr.next = l2;
+                l2 = l2.next;
+            }    
+            curr = curr.next;
+        }
+        curr.next = l1==null?l2:l1;
+        return res.next;
+    }
+
+}
